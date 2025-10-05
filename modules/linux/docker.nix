0e6f345128852e736c0d7a1219dd733d2078025f
Max Baz <max@baz.nu>
2025-10-06 00:42:09 +0200

    docker: switch to podman

--- modules/linux/docker.nix
+++ modules/linux/docker.nix
@@ -1,9 +1,17 @@
-{
-  virtualisation.docker.rootless = {
-    enable = true;
-    setSocketVariable = true;
-    daemon.settings = {
-      dns = [ "1.1.1.1" "8.8.8.8" ];
+{ config, pkgs, ... }: {
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
+
+  virtualisation = {
+    oci-containers.backend = "podman";
+    containers.storage.settings.storage.driver = "btrfs";
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
     };
   };
+
+  systemd.user.services.podman-restart.wantedBy = [ "default.target" ];
+
+  home-manager.users.${config.user}.home.packages = [ pkgs.podman-compose ];
 }
