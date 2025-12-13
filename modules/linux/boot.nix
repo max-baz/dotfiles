f614a02f61dbd8d189d61a6ca1b6ce2f548fbe10
Max Baz <max@baz.nu>
2025-12-13 13:10:02 +0100

    add silent boot with systemd boot

--- modules/linux/boot.nix
+++ modules/linux/boot.nix
@@ -1,4 +1,24 @@
 { lib, ... }: {
+  # silent systemd boot
+  boot = {
+    loader.systemd-boot.enable = true;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "systemd.show_status=false"
+      "rd.systemd.show_status=false"
+      "udev.log_priority=3"
+      "rd.udev.log_priority=3"
+      "vt.global_cursor_default=0"
+    ];
+    consoleLogLevel = 3;
+    plymouth.enable = true;
+    initrd = {
+      systemd.enable = true;
+      verbose = false;
+    };
+  };
+
   systemd.mounts = [{
     what = "tmpfs";
     where = "/tmp";
