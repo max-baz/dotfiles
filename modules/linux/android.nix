f04265500a63d486c354a2c6bb85af5a736b2b44
Max Baz <max@baz.nu>
2026-01-27 22:00:44 +0100

    fix installing android tools

--- modules/linux/android.nix
+++ modules/linux/android.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
-  programs.adb.enable = true;
+{ config, pkgs, ... }: {
+  home-manager.users.${config.user}.home.packages = with pkgs; [ android-tools ];
   users.users.${config.user}.extraGroups = [ "adbusers" ];
 }
