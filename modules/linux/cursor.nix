95bec4c90e048554c2578490ff3e8f85b4bc75d7
Max Baz <max@baz.nu>
2026-07-26 22:57:32 +0200

    enable pointer cursor explicitly to resolve HM warning

--- modules/linux/cursor.nix
+++ modules/linux/cursor.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }: {
   home-manager.users.${config.user}.home.pointerCursor = {
+    enable = true;
     name = "Adwaita";
     package = pkgs.adwaita-icon-theme;
     size = 24;
